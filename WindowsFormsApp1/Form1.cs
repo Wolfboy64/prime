5b308362ab70a17918bbc83e4f59a4e2b1e223a1
Wolfboy64 <ricsi.minecraft1116@gmail.com>
2025-02-06 11:13:29 +0100

    v1.0

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -20,15 +20,15 @@ namespace WindowsFormsApp1
         public Button btn = new Button();
         public Label lbl = new Label();
         public List<int> lista = new List<int>();
-        public void Setup() 
+        public void Setup()
         {
-            
-            lbl.Location = new Point(100,100);
+
+            lbl.Location = new Point(100, 100);
             lbl.Visible = true;
-            lbl.Size = new Size(200,600);
+            lbl.Size = new Size(200, 600);
             Controls.Add(lbl);
 
-            
+
             btn.Location = new Point(lbl.Location.X);
             btn.Visible = true;
             btn.Text = "pím-e? ";
@@ -39,7 +39,7 @@ namespace WindowsFormsApp1
 
         private void Btn_Click(object sender, EventArgs e)
         {
-            int keresett = 17;
+            int keresett = 34;
             /*if (prime(keresett) == true)
             {
                 lbl.Text += "A " + keresett.ToString() + " primszám";
@@ -50,34 +50,59 @@ namespace WindowsFormsApp1
             }*/
 
             int local_ = 0;
-            while (lista.Count() < 50 )
+            while (lista.Count() < 50)
             {
                 if (prime(local_) == true)
                 {
                     lista.Add(local_);
                 }
-                local_++; 
+                local_++;
             }
             /*for (int i = 0; i < lista.Count; i++)
             {
                lbl.Text += "\n" + lista[i];
             }*/
 
-            lbl.Text += "\n" + lista[28];
+            //lbl.Text += "\n" + lista[28];
+            for (int i = 0; i < Felbontas(keresett).Count; i++)
+            {
+                lbl.Text += Felbontas(keresett)[i] + "; ";
+            }
+            
         }
 
         public bool prime(int x)
         {
             if (x <= 1) { return false; }
-            for (int i = 2; i <= Math.Sqrt(x); i++) 
+            for (int i = 2; i <= Math.Sqrt(x); i++)
             {
                 if (x % i == 0)
                 {
-                    return false; 
+                    return false;
                 }
             }
-            return true; 
+            return true;
         }
+        public static List<int> Felbontas(int x)
+        {
+            List<int> osztok = new List<int>();  // Üres lista az osztók tárolásához
 
+            if (x == 1)
+            {
+                osztok.Add(1);  // 1-nek csak 1 az osztója
+            }
+            else
+            {
+                for (int i = 1; i <= x; i++)  // Végigmegyünk 1-től x-ig
+                {
+                    if (x % i == 0)  // Ha i osztója x-nek
+                    {
+                        osztok.Add(i);  // Hozzáadjuk i-t az osztók listájához
+                    }
+                }
+            }
+
+            return osztok;  // Visszaadjuk az osztók listáját
+        }
     }
 }
